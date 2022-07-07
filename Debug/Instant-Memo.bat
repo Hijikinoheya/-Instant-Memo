a5fa571401515ca2b418c99143691af9b39e223f
Hisaki714.github.io <87563197+Hisaki714@users.noreply.github.com>
2022-07-07 16:54:25 +0800

    Add files via upload

---
+++ Debug/Instant-Memo.bat
@@ -0,0 +1,19 @@
+﻿
+REM Created by DecSoft HTML Compiler 2022.6
+REM More information at https://www.decsoftutils.com/
+
+@ECHO OFF
+CLS
+
+REM Set our app file in APP variable
+SET APP="C:\Users\hisak_pf0jq0o\Desktop\NB\Instant-Memo.dhc"
+
+REM Set DecSoft HTML Compiler CLI path
+SET COMPILER="C:\Program Files\DecSoft\HtmlCompiler\HtmlCompilerCLI.exe"
+
+REM Call the compiler with the app file
+%COMPILER% %APP% /debug
+
+REM Pause the script execution to see the results
+PAUSE
+
